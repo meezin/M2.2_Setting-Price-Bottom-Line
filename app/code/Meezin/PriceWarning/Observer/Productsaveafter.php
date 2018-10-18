627fe2bbdfb5077e1a4ef27b79e473947a2b6f9d
Minjae Lee <kkrleemj@gmail.com>
2018-10-18 10:39:57 -0400

    Update app/code/Meezin/PriceWarning/Observer/Productsaveafter.php

--- app/code/Meezin/PriceWarning/Observer/Productsaveafter.php
+++ app/code/Meezin/PriceWarning/Observer/Productsaveafter.php
@@ -6,14 +6,15 @@ class Productsaveafter implements \Magento\Framework\Event\ObserverInterface
   public function execute(\Magento\Framework\Event\Observer $observer)
   {
         $_product = $observer->getProduct();  // you will get product object
-        $producType = $_product->getProductType();
+        $producType = $_product->getTypeId();
         $price = $_product->getPrice(); // will get price
         
-        if($producType=="simple" && $price < 1)
-        {
+        if($producType=='simple' && $price < 30)
+            {
            	
-            throw new \Magento\Framework\Exception\CouldNotDeleteException(__("Prices have been changed below 1!"));
-        }
+            throw new \Magento\Framework\Exception\CouldNotDeleteException(__('Prices have been changed below 30!'));
+            }
+        
 
      return $this;
   }
